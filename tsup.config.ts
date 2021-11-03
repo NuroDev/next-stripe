b43b7bb122a56fbf652360d7d7868a969f1b5e4f
nuro <4991309+NuroDev@users.noreply.github.com>
2021-11-03 11:44:40 +0000

    Renamed src/server to src/index

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 export const tsup: Options = {
   clean: true,
   dts: true,
-  entryPoints: ["src/client.ts", "src/server.ts"],
+  entryPoints: ["src/client.ts", "src/index.ts"],
   format: ["cjs", "esm", "iife"],
   minify: isProduction,
   sourcemap: true,
